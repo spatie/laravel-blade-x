9d32a0df8f98ec2cf283b387b0e23fbac4dfe5ec
freek <freek@spatie.be>
2018-09-28 14:25:43 +0200

    commit

--- src/BladeX.php
+++ src/BladeX.php
@@ -4,4 +4,16 @@ namespace Spatie\BladeX;
 
 class BladeX
 {
+    /** @var \Illuminate\Support\Collection */
+    public $registeredComponents;
+
+    public function component(string $classOrView)
+    {
+
+    }
+
+    public function components()
+    {
+
+    }
 }
