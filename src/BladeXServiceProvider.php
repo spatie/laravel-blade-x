c848d946b83a8e0d70d1c233143ad9ba7e9539e1
freek <freek@spatie.be>
2018-09-28 18:41:08 +0200

    commit

--- src/BladeXServiceProvider.php
+++ src/BladeXServiceProvider.php
@@ -8,23 +8,12 @@ class BladeXServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/BladeX.php' => config_path('BladeX.php'),
-            ], 'config');
-        }
-
         $this->app->singleton(BladeX::class);
 
         $this->app->alias(BladeX::class, 'blade-x');
 
-        $this->app['blade.compiler']->extend(function ($view, $compiler) {
+        $this->app['blade.compiler']->extend(function ($view) {
             return $this->app[BladeX::class]->compile($view);
         });
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-x.php', 'BladeX');
     }
 }
