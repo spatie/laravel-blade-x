91d846bd9d7f6be954d671723796d2ac6d15fe70
freek <freek@spatie.be>
2018-09-28 16:08:20 +0200

    commit

--- src/BladeX.php
+++ src/BladeX.php
@@ -4,11 +4,8 @@ namespace Spatie\BladeX;
 
 use Illuminate\Support\Facades\File;
 use Spatie\BladeX\Exceptions\CouldNotRegisterComponent;
-<<<<<<< HEAD
 use Symfony\Component\Finder\SplFileInfo;
-=======
 use Symfony\Component\DomCrawler\Crawler;
->>>>>>> f490f238acb8e8fa9295d33f2f52f91948a5df34
 
 class BladeX
 {
--- tests/BladeXTest.php
+++ tests/BladeXTest.php
@@ -8,6 +8,12 @@ use Spatie\BladeX\Facades\BladeX;
 class BladeXTest extends TestCase
 {
     /** @test */
+    public function it_can_register_a_single_component()
+    {
+        BladeX::component('data-table', 'data-table');
+    }
+
+    /** @test */
     public function it_can_register_a_directory_containing_view_components()
     {
         BladeX::components($this->getStub('views'));
