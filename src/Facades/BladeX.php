a68ff2188259c33eddf3baf2fe3f5e4e8ad51692
Gummibeer <dev.gummibeer@gmail.com>
2019-08-22 12:12:25 +0200

    improve bladex facade

--- src/Facades/BladeX.php
+++ src/Facades/BladeX.php
@@ -4,7 +4,15 @@ namespace Spatie\BladeX\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/** @see \Spatie\BladeX\BladeX */
+/**
+ * @see \Spatie\BladeX\BladeX
+ *
+ * @method static null|\Spatie\BladeX\Component component($view, string $tag = '')
+ * @method static \Spatie\BladeX\Component[] registeredComponents()
+ * @method static \Spatie\BladeX\BladeX prefix(string $prefix = '')
+ * @method static string getPrefix()
+ * @method static \Spatie\BladeX\ComponentCollection|\Spatie\BladeX\Component[] registerComponents(string $viewDirectory)
+ */
 class BladeX extends Facade
 {
     protected static function getFacadeAccessor()
