9d16765a85bb0e3876cb34adca44a62b46d56397
Gonçalo Marrafa <goncalo.marrafa@gmail.com>
2020-01-06 13:57:39 +0000

    Added test for namespaced attributes

---
+++ tests/Features/ComponentCompilation/stubs/views/namespacedAttributes.blade.php
@@ -0,0 +1,4 @@
+<text-field name="email" label="Email"
+            wire:model.lazy="email" wire.click="validate"
+            ns1:attr1="ns1's value 1" ns1:attr2="ns1's value 2"
+            ns2:attr="ns2's value" />
